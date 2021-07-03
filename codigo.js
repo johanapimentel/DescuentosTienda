f95d13b3d2f55aaf3f0fe8643e597b45f055504f
Vanessa Pimentel <vanessapimentel@Vanessas-iMac.local>
2021-07-02 23:08:23 -0500

    funcion capaz de en base a un precio y un descuento calcular el monto total a pagar por un articulo

---
+++ codigo.js
@@ -0,0 +1,18 @@
+// formula ((precio * (100 - descuento)) / 100) 
+
+document.getElementById("Calcular").addEventListener("click",Calcular);
+
+function Calcular(){
+    var precio = document.getElementById("precioHtml").value;
+    var descuento = document.getElementById("descuentoHtml").value;
+    var alerta = document.getElementById("alerta");
+    if ((precio > 0) && (descuento > 0)){
+        var formula = ((precio * (100 - descuento)) / 100);
+        monto = document.getElementById("montoHtml");
+        monto.innerHTML = formula;
+        alerta.innerHTML = "";
+    } 
+    else {
+        alerta.innerHTML = " El precio no puede ser igual o menor a 0 <br> El descuento no puede ser igual o menor a cero";
+    }
+}
