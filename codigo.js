ee908c4126f8a2eeee030509d08a7133efdd162d
Vanessa Pimentel <vanessapimentel@Vanessas-iMac.local>
2021-07-03 06:32:01 -0500

    se agrupo el codigo para hacerlo mas legible

--- codigo.js
+++ codigo.js
@@ -1,14 +1,8 @@
 // formula ((precio * (100 - descuento)) / 100) 
 
-document.getElementById("Calcular").addEventListener("click",Calcular);
-
-function Calcular(){
-    var precio = document.getElementById("precioHtml").value;
-    var descuento = document.getElementById("descuentoHtml").value;
-    var alerta = document.getElementById("alerta");
+function calcularPrecio(descuento, precio, alerta, monto) {
     if ((precio > 0) && (descuento > 0)){
         var formula = ((precio * (100 - descuento)) / 100);
-        monto = document.getElementById("montoHtml");
         monto.innerHTML = formula;
         alerta.innerHTML = "";
     } 
@@ -16,6 +10,25 @@ function Calcular(){
         alerta.innerHTML = " * El precio no puede ser igual o menor a 0 <br> ** El descuento no puede ser igual o menor a cero";
     }
 }
+
+document.getElementById("calcular").addEventListener("click",descuento);
+
+function descuento(){
+    var precio = document.getElementById("precioHtml").value;
+    var descuento = document.getElementById("descuentoHtml").value;
+    var alerta = document.getElementById("alerta");
+    var monto = document.getElementById("montoHtml");
+    calcularPrecio(descuento, precio, alerta, monto);
+}
+
+function cupones() {
+    var descuento = document.getElementById("opcionesHtml").value
+    var precio = document.getElementById("priceHtml").value;
+    var alerta = document.getElementById("alertaCupon");
+    var monto = document.getElementById("resultadoCuponesHtml");
+    calcularPrecio(descuento, precio, alerta, monto);
+}
+
 /*
 function calculos() {
     alert("Hola!");
